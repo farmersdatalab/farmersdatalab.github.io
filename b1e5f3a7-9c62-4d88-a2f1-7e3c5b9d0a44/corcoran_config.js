0e314b9b981fb0128ff1bc4c3cfba511b47b7cc4
farmersdatalab <farmersdatalab@gmail.com>
2026-04-07 08:21:20 -0400

    Add files via upload

---
+++ b1e5f3a7-9c62-4d88-a2f1-7e3c5b9d0a44/corcoran_config.js
@@ -0,0 +1,314 @@
+window.FIELD_MAP_CONFIG = {
+  "pageTitle": "Corcoran OFE Information",
+  "plotImage": "Plots/aoi_002_Corcoran_2025-05-27.png",
+  "fieldBoundaryGeojsonPolygonCoords": [
+    [
+      [
+        -77.9251795495687,
+        42.927259026326126
+      ],
+      [
+        -77.92533124550548,
+        42.92220238743224
+      ],
+      [
+        -77.92172201512773,
+        42.922067912081296
+      ],
+      [
+        -77.92171867243228,
+        42.92327855497467
+      ],
+      [
+        -77.919782007276,
+        42.92333205744387
+      ],
+      [
+        -77.91972453844431,
+        42.925612935161496
+      ],
+      [
+        -77.92096966455499,
+        42.92561353390846
+      ],
+      [
+        -77.92228311718192,
+        42.92566095764132
+      ],
+      [
+        -77.92225623766883,
+        42.927305410565346
+      ],
+      [
+        -77.92228207512724,
+        42.927385724136165
+      ],
+      [
+        -77.9251795495687,
+        42.927259026326126
+      ]
+    ]
+  ],
+  "areaConfigs": [
+    {
+      "id": "nrate: 60_1",
+      "name": "NRate: 60",
+      "type": "nrate: 60",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -77.92457762625705,
+            42.92218369691324
+          ],
+          [
+            -77.92432719923117,
+            42.927293744521855
+          ],
+          [
+            -77.91964945402503,
+            42.92749777102695
+          ],
+          [
+            -77.91975005460196,
+            42.92334783093501
+          ],
+          [
+            -77.92171659141943,
+            42.92328130267916
+          ],
+          [
+            -77.9217512533721,
+            42.92210148866089
+          ],
+          [
+            -77.92457762625705,
+            42.92218369691324
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 1,
+        "Name": "Tom Corcoran 2025",
+        "NRate": 60
+      }
+    },
+    {
+      "id": "nrate: na_1",
+      "name": "NRate: NA",
+      "type": "nrate: na",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -77.92492424247958,
+            42.92726768979325
+          ],
+          [
+            -77.92515072278334,
+            42.92220035751251
+          ],
+          [
+            -77.92545351180384,
+            42.92220915878487
+          ],
+          [
+            -77.92518502395895,
+            42.927256308410726
+          ],
+          [
+            -77.92492424247958,
+            42.92726768979325
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 2,
+        "Name": "Tom Corcoran 2025",
+        "NRate": null
+      }
+    },
+    {
+      "id": "nrate:50_1",
+      "name": "NRate:50",
+      "type": "nrate:50",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -77.92453693320644,
+            42.92728459217199
+          ],
+          [
+            -77.92477167316119,
+            42.92218933841131
+          ],
+          [
+            -77.92496786590388,
+            42.92219504195861
+          ],
+          [
+            -77.92470040564771,
+            42.92727745831015
+          ],
+          [
+            -77.92453693320644,
+            42.92728459217199
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 3,
+        "Name": "Tom Corcoran 2025",
+        "NRate": 50
+      }
+    },
+    {
+      "id": "nrate:40_1",
+      "name": "NRate:40",
+      "type": "nrate:40",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -77.92432719923117,
+            42.927293744521855
+          ],
+          [
+            -77.92457762625705,
+            42.92218369691324
+          ],
+          [
+            -77.92477167316119,
+            42.92218933841131
+          ],
+          [
+            -77.92453693320644,
+            42.92728459217199
+          ],
+          [
+            -77.92432719923117,
+            42.927293744521855
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 4,
+        "Name": "Tom Corcoran 2025",
+        "NRate": 40
+      }
+    },
+    {
+      "id": "none_1",
+      "name": "None",
+      "type": "none",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -77.92515072278334,
+            42.92220035751251
+          ],
+          [
+            -77.92492424247958,
+            42.92726768979325
+          ],
+          [
+            -77.92470040564771,
+            42.92727745831015
+          ],
+          [
+            -77.92496786590388,
+            42.92219504195861
+          ],
+          [
+            -77.92515072278334,
+            42.92220035751251
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 5,
+        "Name": "Tom Corcoran 2025",
+        "NRate": 60
+      }
+    }
+  ],
+  "rasterFiles": [
+    {
+      "name": "NDVI 2025-05-27",
+      "url": "Rasters/Sentinel2_2025-05-27_1d1b_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-05-27.png"
+    },
+    {
+      "name": "NDVI 2025-06-01",
+      "url": "Rasters/Sentinel2_2025-06-01_da6a_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-06-01.png"
+    },
+    {
+      "name": "NDVI 2025-07-06",
+      "url": "Rasters/Sentinel2_2025-07-06_e191_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-07-06.png"
+    },
+    {
+      "name": "NDVI 2025-07-26",
+      "url": "Rasters/Sentinel2_2025-07-26_8eb8_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-07-26.png"
+    },
+    {
+      "name": "NDVI 2025-07-28",
+      "url": "Rasters/Sentinel2_2025-07-28_8289_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-07-28.png"
+    },
+    {
+      "name": "NDVI 2025-08-05",
+      "url": "Rasters/Sentinel2_2025-08-05_b3eb_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-08-05.png"
+    },
+    {
+      "name": "NDVI 2025-08-10",
+      "url": "Rasters/Sentinel2_2025-08-10_0849_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-08-10.png"
+    },
+    {
+      "name": "NDVI 2025-08-15",
+      "url": "Rasters/Sentinel2_2025-08-15_6557_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-08-15.png"
+    },
+    {
+      "name": "NDVI 2025-09-09",
+      "url": "Rasters/Sentinel2_2025-09-09_45a6_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-09-09.png"
+    },
+    {
+      "name": "NDVI 2025-09-19",
+      "url": "Rasters/Sentinel2_2025-09-19_8a70_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-09-19.png"
+    },
+    {
+      "name": "NDVI 2025-09-29",
+      "url": "Rasters/Sentinel2_2025-09-29_6872_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-09-29.png"
+    },
+    {
+      "name": "NDVI 2025-10-04",
+      "url": "Rasters/Sentinel2_2025-10-04_0106_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-10-04.png"
+    },
+    {
+      "name": "NDVI 2025-10-09",
+      "url": "Rasters/Sentinel2_2025-10-09_693e_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-10-09.png"
+    },
+    {
+      "name": "NDVI 2025-10-14",
+      "url": "Rasters/Sentinel2_2025-10-14_635a_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-10-14.png"
+    },
+    {
+      "name": "NDVI 2025-10-16",
+      "url": "Rasters/Sentinel2_2025-10-16_9acc_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-10-16.png"
+    },
+    {
+      "name": "NDVI 2025-10-24",
+      "url": "Rasters/Sentinel2_2025-10-24_8178_ndvi.tif",
+      "plotImage": "Plots/aoi_002_Corcoran_2025-10-24.png"
+    }
+  ]
+};
