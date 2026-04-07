86e9a06b818bd451be4f54f6b62fb9d58e41f4b6
farmersdatalab <farmersdatalab@gmail.com>
2026-04-07 08:16:45 -0400

    Add files via upload

---
+++ 3f1c9c5a-2e47-4c6d-9b6e-6d7a1f4a8c21/benson_config.js
@@ -0,0 +1,408 @@
+window.FIELD_MAP_CONFIG = {
+  "pageTitle": "Example Farm OFE Information",
+  "plotImage": "Plots/aoi_005_Benson_2025-06-15.png",
+  "fieldBoundaryGeojsonPolygonCoords": [
+    [
+      [
+        -76.4302203178333,
+        42.636220526606564
+      ],
+      [
+        -76.4303582887465,
+        42.63621880374371
+      ],
+      [
+        -76.43051906031636,
+        42.636208304265196
+      ],
+      [
+        -76.43061357492682,
+        42.63619226335999
+      ],
+      [
+        -76.43066477911384,
+        42.636168271659706
+      ],
+      [
+        -76.43075841831809,
+        42.63611402896046
+      ],
+      [
+        -76.43084338557013,
+        42.63605777159194
+      ],
+      [
+        -76.43092830403509,
+        42.63599939184695
+      ],
+      [
+        -76.4310051828336,
+        42.63596658755477
+      ],
+      [
+        -76.43105024882095,
+        42.63592568898669
+      ],
+      [
+        -76.43108907960575,
+        42.63586363907649
+      ],
+      [
+        -76.43111076135114,
+        42.635806049264914
+      ],
+      [
+        -76.43116070035983,
+        42.635726877156905
+      ],
+      [
+        -76.43121029874598,
+        42.635632848822596
+      ],
+      [
+        -76.43123178573182,
+        42.63556676972843
+      ],
+      [
+        -76.43123402773256,
+        42.63553914371645
+      ],
+      [
+        -76.43106371083263,
+        42.635509427919516
+      ],
+      [
+        -76.43093719033834,
+        42.63550888565916
+      ],
+      [
+        -76.43073291622505,
+        42.635502945736704
+      ],
+      [
+        -76.43044830589496,
+        42.63550437752628
+      ],
+      [
+        -76.43020091640537,
+        42.6354989750931
+      ],
+      [
+        -76.43001700589437,
+        42.63550339423914
+      ],
+      [
+        -76.42986456753943,
+        42.63550105144524
+      ],
+      [
+        -76.42974662152888,
+        42.63549827790698
+      ],
+      [
+        -76.42967476248957,
+        42.63549917485629
+      ],
+      [
+        -76.42961644925715,
+        42.63546381300139
+      ],
+      [
+        -76.42947843138364,
+        42.635463412629704
+      ],
+      [
+        -76.42906457202416,
+        42.63547069978494
+      ],
+      [
+        -76.42793241306228,
+        42.635499679943266
+      ],
+      [
+        -76.42692974472948,
+        42.635533404585296
+      ],
+      [
+        -76.42660714552552,
+        42.63553954592362
+      ],
+      [
+        -76.42638683727371,
+        42.635586871030675
+      ],
+      [
+        -76.42629314647769,
+        42.6356389878852
+      ],
+      [
+        -76.42641827402669,
+        42.636333748477874
+      ],
+      [
+        -76.42854192373714,
+        42.6362839300941
+      ],
+      [
+        -76.4293845976359,
+        42.636262802654386
+      ],
+      [
+        -76.42993326719329,
+        42.63624109389879
+      ],
+      [
+        -76.4302203178333,
+        42.636220526606564
+      ]
+    ]
+  ],
+  "areaConfigs": [
+    {
+      "id": "treatment_1",
+      "name": "Cover Crop",
+      "type": "treatment",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -76.43108351938056,
+            42.635886323447664
+          ],
+          [
+            -76.42637432403366,
+            42.63601091516252
+          ],
+          [
+            -76.4263617103555,
+            42.63558332233982
+          ],
+          [
+            -76.42938513000165,
+            42.635448768625345
+          ],
+          [
+            -76.4312375872711,
+            42.6355059580055
+          ],
+          [
+            -76.43123758868624,
+            42.63555979747559
+          ],
+          [
+            -76.43122386699926,
+            42.63556989262229
+          ],
+          [
+            -76.43108351938056,
+            42.635886323447664
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "Name": "Thane_Kolb2"
+      }
+    },
+    {
+      "id": "control_1",
+      "name": "No Cover Crop",
+      "type": "control",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -76.42637432403366,
+            42.63601091516252
+          ],
+          [
+            -76.43108351938056,
+            42.635886323447664
+          ],
+          [
+            -76.431082081004,
+            42.63588956642608
+          ],
+          [
+            -76.43053320573755,
+            42.63620587896014
+          ],
+          [
+            -76.43003921218813,
+            42.63622270696599
+          ],
+          [
+            -76.42638454145748,
+            42.63635727074248
+          ],
+          [
+            -76.42637432403366,
+            42.63601091516252
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "Name": "Thane_Kolb2"
+      }
+    }
+  ],
+  "rasterFiles": [
+    // { <- may be too large to render correctly in browser (several hundred mb in size)
+    //   "name": "RGB UAV 2025-09-12",
+    //   "url": "Rasters/Thane_RGB_Visual.tif",
+    //   "plotImage": "Plots/NoDensityPlot.png"
+    // },
+    {
+      "name": "NDVI 2025-06-15",
+      "url": "Rasters/Sentinel2_2025-06-15_7b3a_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-06-15.png"
+    },
+    {
+      "name": "NDVI 2025-06-18",
+      "url": "Rasters/Sentinel2_2025-06-18_0a19_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-06-18.png"
+    },
+    {
+      "name": "NDVI 2025-06-21",
+      "url": "Rasters/Sentinel2_2025-06-21_6eab_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-06-21.png"
+    },
+    {
+      "name": "NDVI 2025-07-01",
+      "url": "Rasters/Sentinel2_2025-07-01_230b_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-01.png"
+    },
+    {
+      "name": "NDVI 2025-07-06",
+      "url": "Rasters/Sentinel2_2025-07-06_308a_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-06.png"
+    },
+    {
+      "name": "NDVI 2025-07-13",
+      "url": "Rasters/Sentinel2_2025-07-13_a257_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-13.png"
+    },
+    {
+      "name": "NDVI 2025-07-18",
+      "url": "Rasters/Sentinel2_2025-07-18_9e10_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-18.png"
+    },
+    {
+      "name": "NDVI 2025-07-21",
+      "url": "Rasters/Sentinel2_2025-07-21_6f82_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-21.png"
+    },
+    {
+      "name": "NDVI 2025-07-23",
+      "url": "Rasters/Sentinel2_2025-07-23_f5dc_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-23.png"
+    },
+    {
+      "name": "NDVI 2025-07-26",
+      "url": "Rasters/Sentinel2_2025-07-26_b591_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-07-26.png"
+    },
+    {
+      "name": "NDVI 2025-08-07",
+      "url": "Rasters/Sentinel2_2025-08-07_2230_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-08-07.png"
+    },
+    {
+      "name": "NDVI 2025-08-10",
+      "url": "Rasters/Sentinel2_2025-08-10_6939_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-08-10.png"
+    },
+    {
+      "name": "NDVI 2025-08-15",
+      "url": "Rasters/Sentinel2_2025-08-15_c0a4_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-08-15.png"
+    },
+    {
+      "name": "NDVI 2025-08-22",
+      "url": "Rasters/Sentinel2_2025-08-22_10b3_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-08-22.png"
+    },
+    {
+      "name": "NDVI 2025-09-03",
+      "url": "Rasters/Sentinel2_2025-09-03_4ba0_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-09-03.png"
+    },
+    {
+      "name": "NDVI 2025-09-04",
+      "url": "Rasters/Sentinel2_2025-09-04_dccb_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-09-04.png"
+    },
+    {
+      "name": "NDVI 2025-09-09",
+      "url": "Rasters/Sentinel2_2025-09-09_fdf7_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-09-09.png"
+    },
+    {
+      "name": "NDVI 2025-09-11",
+      "url": "Rasters/Sentinel2_2025-09-11_16f1_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-09-11.png"
+    },
+    {
+      "name": "NDVI 2025-09-14",
+      "url": "Rasters/Sentinel2_2025-09-14_474d_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-09-14.png"
+    },
+    {
+      "name": "NDVI 2025-09-16",
+      "url": "Rasters/Sentinel2_2025-09-16_9cf5_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-09-16.png"
+    },
+    {
+      "name": "NDVI 2025-10-01",
+      "url": "Rasters/Sentinel2_2025-10-01_8739_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-01.png"
+    },
+    {
+      "name": "NDVI 2025-10-04",
+      "url": "Rasters/Sentinel2_2025-10-04_62c9_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-04.png"
+    },
+    {
+      "name": "NDVI 2025-10-06",
+      "url": "Rasters/Sentinel2_2025-10-06_2fba_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-06.png"
+    },
+    {
+      "name": "NDVI 2025-10-09",
+      "url": "Rasters/Sentinel2_2025-10-09_0536_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-09.png"
+    },
+    {
+      "name": "NDVI 2025-10-16",
+      "url": "Rasters/Sentinel2_2025-10-16_5be9_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-16.png"
+    },
+    {
+      "name": "NDVI 2025-10-21",
+      "url": "Rasters/Sentinel2_2025-10-21_104e_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-21.png"
+    },
+    {
+      "name": "NDVI 2025-10-29",
+      "url": "Rasters/Sentinel2_2025-10-29_b611_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-10-29.png"
+    },
+    {
+      "name": "NDVI 2025-11-02",
+      "url": "Rasters/Sentinel2_2025-11-02_a072_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-11-02.png"
+    },
+    {
+      "name": "NDVI 2025-11-08",
+      "url": "Rasters/Sentinel2_2025-11-08_419e_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-11-08.png"
+    },
+    {
+      "name": "NDVI 2025-11-15",
+      "url": "Rasters/Sentinel2_2025-11-15_4e40_ndvi.tif",
+      "plotImage": "Plots/aoi_005_Benson_2025-11-15.png"
+    },
+    {
+      "name": "Yield 2025-11-15",
+      "url": "Rasters/Benson_Yield_4326.tif",
+      "plotImage": "Plots/aoi_005_Benson_yieldDensity.png"
+    }
+  ]
+};
