ce952bf5d492cd20245ee8f5b77d0e6f14a6671b
farmersdatalab <farmersdatalab@gmail.com>
2026-04-07 08:20:09 -0400

    Add files via upload

---
+++ 8a7d2e11-5c39-4f0f-9c3a-2d6b7e1a4f90/branton_config.js
@@ -0,0 +1,426 @@
+window.FIELD_MAP_CONFIG = {
+  "pageTitle": "Branton OFE Information",
+  "plotImage": "Plots/aoi_001_Branton_2025-06-11.png",
+  "fieldBoundaryGeojsonPolygonCoords": [
+    [
+      [
+        -78.05516730594083,
+        42.99751279141099
+      ],
+      [
+        -78.05519854515825,
+        42.99467764781032
+      ],
+      [
+        -78.04517156460895,
+        42.994738400382246
+      ],
+      [
+        -78.04518528660064,
+        42.995981768505864
+      ],
+      [
+        -78.04542769152467,
+        42.99596129909335
+      ],
+      [
+        -78.04583675417682,
+        42.99593288675786
+      ],
+      [
+        -78.04622319430624,
+        42.9959295999243
+      ],
+      [
+        -78.04656594751282,
+        42.99600805561024
+      ],
+      [
+        -78.04685555984555,
+        42.99626660337213
+      ],
+      [
+        -78.04681125904403,
+        42.99643244702117
+      ],
+      [
+        -78.04651207682525,
+        42.99655953625413
+      ],
+      [
+        -78.04621437319722,
+        42.99662001931218
+      ],
+      [
+        -78.04586164121666,
+        42.996629415235006
+      ],
+      [
+        -78.04563353382822,
+        42.99664950477757
+      ],
+      [
+        -78.04540594509584,
+        42.99668009047425
+      ],
+      [
+        -78.04522775203576,
+        42.996744395039634
+      ],
+      [
+        -78.04519463377552,
+        42.996846877739486
+      ],
+      [
+        -78.045232258237,
+        42.997511535836054
+      ],
+      [
+        -78.05516730594083,
+        42.99751279141099
+      ]
+    ]
+  ],
+  "areaConfigs": [
+  {
+    "id": "62 lbs n / ac_1",
+    "name": "62 Lbs N / Ac",
+    "type": "62 lbs n / ac",
+    "geojsonPolygonCoords": [
+      [
+        [-78.04520394150437, 42.99507521491573],
+        [-78.0552342703745, 42.995075214175422],
+        [-78.055229855098915, 42.995189748825638],
+        [-78.04520727328918, 42.995189750498795],
+        [-78.04520394150437, 42.99507521491573]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 1,
+      "Name": "Branton_Field302-3",
+      "NRate": "62"
+    }
+  },
+  {
+    "id": "50 lbs n / ac_1",
+    "name": "50 Lbs N / Ac",
+    "type": "50 lbs n / ac",
+    "geojsonPolygonCoords": [
+      [
+        [-78.04520727328918, 42.995189750498795],
+        [-78.055229855098915, 42.995189748825638],
+        [-78.055225860094339, 42.995293381319826],
+        [-78.045210445124823, 42.995298786780744],
+        [-78.04520727328918, 42.995189750498795]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 2,
+      "Name": "Branton_Field302-3",
+      "NRate": "50"
+    }
+  },
+  {
+    "id": "37 lbs n / ac_1",
+    "name": "37 Lbs N / Ac",
+    "type": "37 lbs n / ac",
+    "geojsonPolygonCoords": [
+      [
+        [-78.055242470382012, 42.994862501222784],
+        [-78.05523847541447, 42.994966133047065],
+        [-78.045200768487405, 42.994966136453009],
+        [-78.045197595527625, 42.994857059189535],
+        [-78.055242470382012, 42.994862501222784]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 3,
+      "Name": "Branton_Field302-3",
+      "NRate": "37"
+    }
+  },
+  {
+    "id": "37 lbs n / ac_2",
+    "name": "37 Lbs N / Ac",
+    "type": "37 lbs n / ac",
+    "geojsonPolygonCoords": [
+      [
+        [-78.055221445062173, 42.995407909441433],
+        [-78.055217240036455, 42.995516989786402],
+        [-78.045216792673642, 42.995516990341656],
+        [-78.045213619512964, 42.995407910042296],
+        [-78.055221445062173, 42.995407909441433]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 3,
+      "Name": "Branton_Field302-3",
+      "NRate": "37"
+    }
+  },
+  {
+    "id": "25 lbs n / ac_1",
+    "name": "25 Lbs N / Ac",
+    "type": "25 lbs n / ac",
+    "geojsonPolygonCoords": [
+      [
+        [-78.05523847541447, 42.994966133047065],
+        [-78.055234270374498, 42.995075214175422],
+        [-78.045203941504369, 42.99507521491573],
+        [-78.045200768487405, 42.994966136453009],
+        [-78.05523847541447, 42.994966133047065]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 4,
+      "Name": "Branton_Field302-3",
+      "NRate": "25"
+    }
+  },
+  {
+    "id": "25 lbs n / ac_2",
+    "name": "25 Lbs N / Ac",
+    "type": "25 lbs n / ac",
+    "geojsonPolygonCoords": [
+      [
+        [-78.055217240036455, 42.995516989786402],
+        [-78.055213035026881, 42.9956260696109],
+        [-78.045219965841483, 42.995626070120643],
+        [-78.045216792673642, 42.995516990341656],
+        [-78.055217240036455, 42.995516989786402]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 4,
+      "Name": "Branton_Field302-3",
+      "NRate": "25"
+    }
+  },
+  {
+    "id": "variable rate n_1",
+    "name": "Variable Rate N",
+    "type": "variable rate n",
+    "geojsonPolygonCoords": [
+      [
+        [-78.045445734509428, 42.99467161328176],
+        [-78.045691811030935, 42.994649796965],
+        [-78.046683573981284, 42.994649796964985],
+        [-78.046787970081297, 42.994633434722331],
+        [-78.055251511048411, 42.994627980640473],
+        [-78.055246884975162, 42.994747983953744],
+        [-78.055242470382012, 42.994862501222784],
+        [-78.045197595527625, 42.994857059189535],
+        [-78.045194739480877, 42.99475887583263],
+        [-78.045192201123626, 42.994671613281731],
+        [-78.045445734509428, 42.99467161328176]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 5,
+      "Name": "Branton_Field302-3",
+      "NRate": "variable"
+    }
+  },
+  {
+    "id": "variable rate n_2",
+    "name": "Variable Rate N",
+    "type": "variable rate n",
+    "geojsonPolygonCoords": [
+      [
+        [-78.055221445062173, 42.995407909441433],
+        [-78.045213619512964, 42.995407910042296],
+        [-78.045210445124823, 42.995298786780744],
+        [-78.055225860094339, 42.995293381319826],
+        [-78.055221445062173, 42.995407909441433]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 5,
+      "Name": "Branton_Field302-3",
+      "NRate": "variable"
+    }
+  },
+  {
+    "id": "variable rate n_3",
+    "name": "Variable Rate N",
+    "type": "variable rate n",
+    "geojsonPolygonCoords": [
+      [
+        [-78.055208829781307, 42.995735155455321],
+        [-78.045223367556815, 42.995746048088527],
+        [-78.045222984604365, 42.995729841414033],
+        [-78.045219965841483, 42.995626070120643],
+        [-78.055213035026881, 42.9956260696109],
+        [-78.055208829781307, 42.995735155455321]
+      ]
+    ],
+    "extraMeta": {
+      "fid": 5,
+      "Name": "Branton_Field302-3",
+      "NRate": "variable"
+    }
+  }
+],
+  "rasterFiles": [
+    {
+      "name": "NDVI 2025-06-11",
+      "url": "Rasters/Sentinel2_2025-06-11_ce44_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-06-11.png"
+    },
+    {
+      "name": "NDVI 2025-06-21",
+      "url": "Rasters/Sentinel2_2025-06-21_6c4d_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-06-21.png"
+    },
+    {
+      "name": "NDVI 2025-06-29",
+      "url": "Rasters/Sentinel2_2025-06-29_d32f_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-06-29.png"
+    },
+    {
+      "name": "NDVI 2025-07-04",
+      "url": "Rasters/Sentinel2_2025-07-04_12ec_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-07-04.png"
+    },
+    {
+      "name": "NDVI 2025-07-06",
+      "url": "Rasters/Sentinel2_2025-07-06_8b7e_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-07-06.png"
+    },
+    {
+      "name": "NDVI 2025-07-19",
+      "url": "Rasters/Sentinel2_2025-07-19_8991_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-07-19.png"
+    },
+    {
+      "name": "NDVI 2025-07-24",
+      "url": "Rasters/Sentinel2_2025-07-24_3f5c_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-07-24.png"
+    },
+    {
+      "name": "NDVI 2025-07-28",
+      "url": "Rasters/Sentinel2_2025-07-28_0879_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-07-28.png"
+    },
+    {
+      "name": "NDVI 2025-07-29",
+      "url": "Rasters/Sentinel2_2025-07-29_41ad_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-07-29.png"
+    },
+    {
+      "name": "NDVI 2025-08-03",
+      "url": "Rasters/Sentinel2_2025-08-03_26ed_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-03.png"
+    },
+    {
+      "name": "NDVI 2025-08-05",
+      "url": "Rasters/Sentinel2_2025-08-05_edcc_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-05.png"
+    },
+    {
+      "name": "NDVI 2025-08-08",
+      "url": "Rasters/Sentinel2_2025-08-08_caee_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-08.png"
+    },
+    {
+      "name": "NDVI 2025-08-10",
+      "url": "Rasters/Sentinel2_2025-08-10_953e_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-10.png"
+    },
+    {
+      "name": "NDVI 2025-08-15",
+      "url": "Rasters/Sentinel2_2025-08-15_b1c0_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-15.png"
+    },
+    {
+      "name": "NDVI 2025-08-18",
+      "url": "Rasters/Sentinel2_2025-08-18_81fc_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-18.png"
+    },
+    {
+      "name": "NDVI 2025-08-25",
+      "url": "Rasters/Sentinel2_2025-08-25_f96f_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-25.png"
+    },
+    {
+      "name": "NDVI 2025-08-30",
+      "url": "Rasters/Sentinel2_2025-08-30_eb4d_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-08-30.png"
+    },
+    {
+      "name": "NDVI 2025-09-09",
+      "url": "Rasters/Sentinel2_2025-09-09_c18d_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-09-09.png"
+    },
+    {
+      "name": "NDVI 2025-09-12",
+      "url": "Rasters/Sentinel2_2025-09-12_7579_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-09-12.png"
+    },
+    {
+      "name": "NDVI 2025-09-17",
+      "url": "Rasters/Sentinel2_2025-09-17_9688_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-09-17.png"
+    },
+    {
+      "name": "NDVI 2025-09-19",
+      "url": "Rasters/Sentinel2_2025-09-19_ba87_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-09-19.png"
+    },
+    {
+      "name": "NDVI 2025-09-29",
+      "url": "Rasters/Sentinel2_2025-09-29_b699_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-09-29.png"
+    },
+    {
+      "name": "NDVI 2025-10-02",
+      "url": "Rasters/Sentinel2_2025-10-02_542f_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-02.png"
+    },
+    {
+      "name": "NDVI 2025-10-04",
+      "url": "Rasters/Sentinel2_2025-10-04_6e7d_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-04.png"
+    },
+    {
+      "name": "NDVI 2025-10-09",
+      "url": "Rasters/Sentinel2_2025-10-09_d2b0_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-09.png"
+    },
+    {
+      "name": "NDVI 2025-10-12",
+      "url": "Rasters/Sentinel2_2025-10-12_e756_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-12.png"
+    },
+    {
+      "name": "NDVI 2025-10-16",
+      "url": "Rasters/Sentinel2_2025-10-16_e504_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-16.png"
+    },
+    {
+      "name": "NDVI 2025-10-22",
+      "url": "Rasters/Sentinel2_2025-10-22_e035_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-22.png"
+    },
+    {
+      "name": "NDVI 2025-10-27",
+      "url": "Rasters/Sentinel2_2025-10-27_5d17_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-27.png"
+    },
+    {
+      "name": "NDVI 2025-10-29",
+      "url": "Rasters/Sentinel2_2025-10-29_9ec2_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-10-29.png"
+    },
+    {
+      "name": "NDVI 2025-11-01",
+      "url": "Rasters/Sentinel2_2025-11-01_18f1_ndvi.tif",
+      "plotImage": "Plots/aoi_001_Branton_2025-11-01.png"
+    },
+    {
+      "name": "Yield 2025-11-02",
+      "url": "Rasters/Branton_Yield_4326.tif",
+      "plotImage": "Plots/aoi_001_Branton_yieldDensity.png"
+    }
+  ]
+};
