8de59517085417f1340891182a91bf3e1bc9ba5e
farmersdatalab <farmersdatalab@gmail.com>
2026-04-07 08:19:15 -0400

    Add files via upload

---
+++ 6d2a9f0c-3b77-4e1e-8a5c-1f9d3b6e7a22/curc16_config.js
@@ -0,0 +1,244 @@
+window.FIELD_MAP_CONFIG = {
+  "pageTitle": "CURC16 OFE Information",
+  "plotImage": "Plots/aoi_009_CURC16_2025-06-15.png",
+  "fieldBoundaryGeojsonPolygonCoords": [
+    [
+      [
+        -76.25101488767183,
+        42.440364012081034
+      ],
+      [
+        -76.24799694395283,
+        42.43887678789439
+      ],
+      [
+        -76.24787897872604,
+        42.43879698462309
+      ],
+      [
+        -76.2467484786359,
+        42.437766787814695
+      ],
+      [
+        -76.2467189873292,
+        42.43767972815336
+      ],
+      [
+        -76.24678780037816,
+        42.43742580345028
+      ],
+      [
+        -76.24727932215649,
+        42.43609086808797
+      ],
+      [
+        -76.24769220045027,
+        42.43512592427722
+      ],
+      [
+        -76.24790847003275,
+        42.43448745704685
+      ],
+      [
+        -76.25130980073884,
+        42.43612714388131
+      ],
+      [
+        -76.25274504433155,
+        42.43686716548166
+      ],
+      [
+        -76.25281385738052,
+        42.43691795095732
+      ],
+      [
+        -76.25251894431354,
+        42.43767247317613
+      ],
+      [
+        -76.25262707910476,
+        42.43833267266511
+      ],
+      [
+        -76.25251894431354,
+        42.43874620067026
+      ],
+      [
+        -76.25239114865117,
+        42.43926129314061
+      ],
+      [
+        -76.25244030082902,
+        42.43992147589019
+      ],
+      [
+        -76.25248945300683,
+        42.44018989985429
+      ],
+      [
+        -76.25229284429551,
+        42.44038577607535
+      ],
+      [
+        -76.25101488767183,
+        42.440364012081034
+      ]
+    ]
+  ],
+  "areaConfigs": [
+    {
+      "id": "none_1",
+      "name": "None",
+      "type": "none",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -76.25282370229637,
+            42.43701647680292
+          ],
+          [
+            -76.24780315806792,
+            42.43445562877885
+          ],
+          [
+            -76.24644761962864,
+            42.43813689826716
+          ],
+          [
+            -76.24863804338479,
+            42.44051376218689
+          ],
+          [
+            -76.25239015410772,
+            42.44040168816476
+          ],
+          [
+            -76.25282370229637,
+            42.43701647680292
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 1,
+        "Name": "CURC2025_Field16"
+      }
+    }
+  ],
+  "rasterFiles": [
+    {
+      "name": "NDVI 2025-06-15",
+      "url": "Rasters/Sentinel2_2025-06-15_a7cf_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-06-15.png"
+    },
+    {
+      "name": "NDVI 2025-06-16",
+      "url": "Rasters/Sentinel2_2025-06-16_929b_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-06-16.png"
+    },
+    {
+      "name": "NDVI 2025-06-21",
+      "url": "Rasters/Sentinel2_2025-06-21_b20b_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-06-21.png"
+    },
+    {
+      "name": "NDVI 2025-06-28",
+      "url": "Rasters/Sentinel2_2025-06-28_b4f7_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-06-28.png"
+    },
+    {
+      "name": "NDVI 2025-07-05",
+      "url": "Rasters/Sentinel2_2025-07-05_7e3f_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-07-05.png"
+    },
+    {
+      "name": "NDVI 2025-07-06",
+      "url": "Rasters/Sentinel2_2025-07-06_0e37_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-07-06.png"
+    },
+    {
+      "name": "NDVI 2025-07-11",
+      "url": "Rasters/Sentinel2_2025-07-11_a352_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-07-11.png"
+    },
+    {
+      "name": "NDVI 2025-07-23",
+      "url": "Rasters/Sentinel2_2025-07-23_8a84_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-07-23.png"
+    },
+    {
+      "name": "NDVI 2025-07-28",
+      "url": "Rasters/Sentinel2_2025-07-28_2a40_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-07-28.png"
+    },
+    {
+      "name": "NDVI 2025-08-02",
+      "url": "Rasters/Sentinel2_2025-08-02_f486_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-02.png"
+    },
+    {
+      "name": "NDVI 2025-08-05",
+      "url": "Rasters/Sentinel2_2025-08-05_3e55_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-05.png"
+    },
+    {
+      "name": "NDVI 2025-08-07",
+      "url": "Rasters/Sentinel2_2025-08-07_e10f_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-07.png"
+    },
+    {
+      "name": "NDVI 2025-08-10",
+      "url": "Rasters/Sentinel2_2025-08-10_3a73_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-10.png"
+    },
+    {
+      "name": "NDVI 2025-08-12",
+      "url": "Rasters/Sentinel2_2025-08-12_45bd_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-12.png"
+    },
+    {
+      "name": "NDVI 2025-08-22",
+      "url": "Rasters/Sentinel2_2025-08-22_d43e_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-22.png"
+    },
+    {
+      "name": "NDVI 2025-08-30",
+      "url": "Rasters/Sentinel2_2025-08-30_2e92_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-08-30.png"
+    },
+    {
+      "name": "NDVI 2025-09-03",
+      "url": "Rasters/Sentinel2_2025-09-03_e614_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-03.png"
+    },
+    {
+      "name": "NDVI 2025-09-04",
+      "url": "Rasters/Sentinel2_2025-09-04_83b1_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-04.png"
+    },
+    {
+      "name": "NDVI 2025-09-09",
+      "url": "Rasters/Sentinel2_2025-09-09_d051_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-09.png"
+    },
+    {
+      "name": "NDVI 2025-09-11",
+      "url": "Rasters/Sentinel2_2025-09-11_c6f4_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-11.png"
+    },
+    {
+      "name": "NDVI 2025-09-14",
+      "url": "Rasters/Sentinel2_2025-09-14_c7b1_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-14.png"
+    },
+    {
+      "name": "NDVI 2025-09-16",
+      "url": "Rasters/Sentinel2_2025-09-16_82cd_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-16.png"
+    },
+    {
+      "name": "NDVI 2025-09-21",
+      "url": "Rasters/Sentinel2_2025-09-21_c87f_ndvi.tif",
+      "plotImage": "Plots/aoi_009_CURC16_2025-09-21.png"
+    }
+  ]
+};
