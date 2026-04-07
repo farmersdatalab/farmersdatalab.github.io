4b32a7fc6f7d616196abe891e81f0213f84c7c45
farmersdatalab <farmersdatalab@gmail.com>
2026-04-07 08:18:33 -0400

    Add files via upload

---
+++ 5c9e1a7d-8f44-4b2a-9d3c-6e7a1f2b0c55/porter_config.js
@@ -0,0 +1,395 @@
+window.FIELD_MAP_CONFIG = {
+  "pageTitle": "Porter OFE Information",
+  "plotImage": "Plots/aoi_008_Porter_2025-06-13.png",
+  "fieldBoundaryGeojsonPolygonCoords": [
+    [
+      [
+        -76.71862329079697,
+        42.56690100541257
+      ],
+      [
+        -76.71934623232833,
+        42.5668845382634
+      ],
+      [
+        -76.71999091730214,
+        42.566879049212716
+      ],
+      [
+        -76.7205163541883,
+        42.56685983753151
+      ],
+      [
+        -76.72110886812379,
+        42.566846114898446
+      ],
+      [
+        -76.72120948369773,
+        42.56688179373811
+      ],
+      [
+        -76.72155604845246,
+        42.566879049212716
+      ],
+      [
+        -76.721790818125,
+        42.566870815635774
+      ],
+      [
+        -76.72196223725099,
+        42.56685983753151
+      ],
+      [
+        -76.72198832277013,
+        42.56678573527714
+      ],
+      [
+        -76.72196969025644,
+        42.56652774896413
+      ],
+      [
+        -76.72194733124,
+        42.56612155558554
+      ],
+      [
+        -76.72193242522907,
+        42.565929436174336
+      ],
+      [
+        -76.72192869872634,
+        42.565874544805325
+      ],
+      [
+        -76.7217237410757,
+        42.56583612081829
+      ],
+      [
+        -76.72149642440863,
+        42.56583063167535
+      ],
+      [
+        -76.7204567301445,
+        42.565860821955525
+      ],
+      [
+        -76.71972260910493,
+        42.565885523083004
+      ],
+      [
+        -76.71958100200085,
+        42.566039218767465
+      ],
+      [
+        -76.71939095036119,
+        42.566209381405
+      ],
+      [
+        -76.71913382167223,
+        42.5662807397923
+      ],
+      [
+        -76.71896612904898,
+        42.56634386445147
+      ],
+      [
+        -76.71869782085177,
+        42.56640424450047
+      ],
+      [
+        -76.71853012822852,
+        42.566464624491026
+      ],
+      [
+        -76.71844814516827,
+        42.56663753050432
+      ],
+      [
+        -76.71847423068745,
+        42.566793968865305
+      ],
+      [
+        -76.71855994025042,
+        42.56686258205775
+      ],
+      [
+        -76.71862329079697,
+        42.56690100541257
+      ]
+    ]
+  ],
+  "areaConfigs": [
+    {
+      "id": "notreat1_1",
+      "name": "NoTreat1",
+      "type": "notreat1",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -76.71900968142666,
+            42.56628529734405
+          ],
+          [
+            -76.7219413619198,
+            42.56622605133611
+          ],
+          [
+            -76.72196602616256,
+            42.56687244447074
+          ],
+          [
+            -76.7185094163345,
+            42.56687945882578
+          ],
+          [
+            -76.71849033567398,
+            42.56649267210675
+          ],
+          [
+            -76.71869086117051,
+            42.566387188811234
+          ],
+          [
+            -76.71900968142666,
+            42.56628529734405
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 1,
+        "Name": "RodPorter_DrivewayFields"
+      }
+    },
+    {
+      "id": "nooootreat2_1",
+      "name": "NooooTreat2",
+      "type": "nooootreat2",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -76.72193539247137,
+            42.56606960292831
+          ],
+          [
+            -76.7193056937509,
+            42.566137730561216
+          ],
+          [
+            -76.71939746557426,
+            42.56606370668481
+          ],
+          [
+            -76.719375729473,
+            42.565882237625864
+          ],
+          [
+            -76.72192531048526,
+            42.56580536982597
+          ],
+          [
+            -76.72193539247137,
+            42.56606960292831
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 2,
+        "Name": "RodPorter_DrivewayFields"
+      }
+    },
+    {
+      "id": "treat_1",
+      "name": "Treat",
+      "type": "treat",
+      "geojsonPolygonCoords": [
+        [
+          [
+            -76.7219413619198,
+            42.56622605133611
+          ],
+          [
+            -76.71900968142666,
+            42.56628529734405
+          ],
+          [
+            -76.7191969418251,
+            42.566225450404524
+          ],
+          [
+            -76.7193056937509,
+            42.566137730561216
+          ],
+          [
+            -76.72193539247137,
+            42.56606960292831
+          ],
+          [
+            -76.7219413619198,
+            42.56622605133611
+          ]
+        ]
+      ],
+      "extraMeta": {
+        "fid": 3,
+        "Name": "RodPorter_DrivewayFields"
+      }
+    }
+  ],
+  "rasterFiles": [
+    
+    {
+      "name": "NDVI 2025-06-13",
+      "url": "Rasters/Sentinel2_2025-06-13_fb6e_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-06-13.png"
+    },
+    {
+      "name": "NDVI 2025-06-18",
+      "url": "Rasters/Sentinel2_2025-06-18_725e_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-06-18.png"
+    },
+    {
+      "name": "NDVI 2025-06-21",
+      "url": "Rasters/Sentinel2_2025-06-21_8445_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-06-21.png"
+    },
+    {
+      "name": "NDVI 2025-06-28",
+      "url": "Rasters/Sentinel2_2025-06-28_6830_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-06-28.png"
+    },
+    {
+      "name": "NDVI 2025-07-05",
+      "url": "Rasters/Sentinel2_2025-07-05_9592_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-07-05.png"
+    },
+    {
+      "name": "NDVI 2025-07-13",
+      "url": "Rasters/Sentinel2_2025-07-13_20f6_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-07-13.png"
+    },
+    {
+      "name": "NDVI 2025-07-18",
+      "url": "Rasters/Sentinel2_2025-07-18_77f1_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-07-18.png"
+    },
+    {
+      "name": "NDVI 2025-07-23",
+      "url": "Rasters/Sentinel2_2025-07-23_2651_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-07-23.png"
+    },
+    {
+      "name": "NDVI 2025-07-26",
+      "url": "Rasters/Sentinel2_2025-07-26_f613_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-07-26.png"
+    },
+    {
+      "name": "NDVI 2025-07-28",
+      "url": "Rasters/Sentinel2_2025-07-28_099d_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-07-28.png"
+    },
+    {
+      "name": "NDVI 2025-08-02",
+      "url": "Rasters/Sentinel2_2025-08-02_7501_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-08-02.png"
+    },
+    {
+      "name": "NDVI 2025-08-07",
+      "url": "Rasters/Sentinel2_2025-08-07_0899_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-08-07.png"
+    },
+    {
+      "name": "NDVI 2025-08-12",
+      "url": "Rasters/Sentinel2_2025-08-12_8267_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-08-12.png"
+    },
+    {
+      "name": "NDVI 2025-08-22",
+      "url": "Rasters/Sentinel2_2025-08-22_fbc1_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-08-22.png"
+    },
+    {
+      "name": "NDVI 2025-08-27",
+      "url": "Rasters/Sentinel2_2025-08-27_a432_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-08-27.png"
+    },
+    {
+      "name": "NDVI 2025-08-30",
+      "url": "Rasters/Sentinel2_2025-08-30_31bf_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-08-30.png"
+    },
+    {
+      "name": "NDVI 2025-09-03",
+      "url": "Rasters/Sentinel2_2025-09-03_b396_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-09-03.png"
+    },
+    {
+      "name": "NDVI 2025-09-09",
+      "url": "Rasters/Sentinel2_2025-09-09_8df9_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-09-09.png"
+    },
+    {
+      "name": "NDVI 2025-09-11",
+      "url": "Rasters/Sentinel2_2025-09-11_79dc_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-09-11.png"
+    },
+    {
+      "name": "NDVI 2025-09-16",
+      "url": "Rasters/Sentinel2_2025-09-16_d9e9_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-09-16.png"
+    },
+    {
+      "name": "NDVI 2025-10-01",
+      "url": "Rasters/Sentinel2_2025-10-01_40d9_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-01.png"
+    },
+    {
+      "name": "NDVI 2025-10-04",
+      "url": "Rasters/Sentinel2_2025-10-04_2415_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-04.png"
+    },
+    {
+      "name": "NDVI 2025-10-06",
+      "url": "Rasters/Sentinel2_2025-10-06_cecc_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-06.png"
+    },
+    {
+      "name": "NDVI 2025-10-09",
+      "url": "Rasters/Sentinel2_2025-10-09_67ff_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-09.png"
+    },
+    {
+      "name": "NDVI 2025-10-16",
+      "url": "Rasters/Sentinel2_2025-10-16_39be_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-16.png"
+    },
+    {
+      "name": "NDVI 2025-10-21",
+      "url": "Rasters/Sentinel2_2025-10-21_ad2e_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-21.png"
+    },
+    {
+      "name": "NDVI 2025-10-29",
+      "url": "Rasters/Sentinel2_2025-10-29_480a_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-10-29.png"
+    },
+    {
+      "name": "NDVI 2025-11-02",
+      "url": "Rasters/Sentinel2_2025-11-02_17eb_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-11-02.png"
+    },
+    {
+      "name": "NDVI 2025-11-08",
+      "url": "Rasters/Sentinel2_2025-11-08_e725_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-11-08.png"
+    },
+    {
+      "name": "NDVI 2025-11-10",
+      "url": "Rasters/Sentinel2_2025-11-10_e1a1_ndvi.tif",
+      "plotImage": "Plots/aoi_008_Porter_2025-11-10.png"
+    },
+    {
+      "name": "Yield 2025-11-12",
+      "url": "Rasters/Porter_Yield_4326.tif",
+      "plotImage": "Plots/aoi_008_Porter_yieldDensity.png"
+    }
+  ]
+};
